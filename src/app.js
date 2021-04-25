38f457efd4544d55c99a3236c5120173016a171c
jordan237prog <lordmoyojordan@gmail.com>
2021-04-25 15:24:51 +0100

    install graphql and added apollo server to express middleware

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
-console.log('welcome to this application');
-
+import apollo from 'apollo-server-express';
+import cors from 'cors';
 import express from 'express';
 
 import config from './Config/index.js';
 
 const app = express();
+app.use(cors());
+const { ApolloServer, gql } = apollo;
 const port = config.port;
+
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Robin',
+      };
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -13,5 +41,6 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
+  console.log(`Apollo Server on http://localhost:${port}/graphql`);
   console.log(`In the ${config.env} environment`);
 });
